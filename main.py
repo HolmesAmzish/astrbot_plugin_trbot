beb229f6bf46d73d9b0b8a6eeb7210eb34d9ce74
Cacciatore <holmesamzish86@outlook.com>
2025-04-22 20:08:27 +0800

    Update playing query

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
 from astrbot.api.star import Context, Star, register
 from astrbot.api import logger
+import requests
+import json
 
-@register("helloworld", "YourName", "一个简单的 Hello World 插件", "1.0.0")
+@register("TShock Bot", "Cacciatore", "A TShock Bot to control server", "1.0")
 class MyPlugin(Star):
     def __init__(self, context: Context):
         super().__init__(context)
@@ -10,15 +12,36 @@ class MyPlugin(Star):
     async def initialize(self):
         """可选择实现异步的插件初始化方法，当实例化该插件类之后会自动调用该方法。"""
     
-    # 注册指令的装饰器。指令名为 helloworld。注册成功后，发送 `/helloworld` 就会触发这个指令，并回复 `你好, {user_name}!`
-    @filter.command("helloworld")
+    @filter.command("playing")
     async def helloworld(self, event: AstrMessageEvent):
-        """这是一个 hello world 指令""" # 这是 handler 的描述，将会被解析方便用户了解插件内容。建议填写。
-        user_name = event.get_sender_name()
-        message_str = event.message_str # 用户发的纯文本消息字符串
-        message_chain = event.get_messages() # 用户所发的消息的消息链 # from astrbot.api.message_components import *
-        logger.info(message_chain)
-        yield event.plain_result(f"Hello, {user_name}, 你发了 {message_str}!") # 发送一条纯文本消息
+        """Query the number of online players""" # 这是 handler 的描述，将会被解析方便用户了解插件内容。建议填写。
+        
+
+        url = "http://localhost:7878/v2/players/list"
+        token = "astrbot"
+        params = {"token": token}
+
+        try:
+            response = requests.get(url, params=params)
+            response.raise_for_status()
+
+            data = response.json()
+
+            if data["status"] == "200" and "players" in data:
+                online_players = [player for player in data["players"] if player.get("active")]
+                online_count = len(online_players)
+            else:
+                print("Cannot get online player list or response format is incorrect.")
+                print("Response", data)
+
+        except requests.exceptions.RequestException as e:
+            print(f"Request error: {e}")
+        except json.JSONDecodeError:
+            print("Cannot parse JSON response.")
+            print("Response:", response.text)
+
+
+        yield event.plain_result(f"服务器当前共 {online_count} 名玩家") # 发送一条纯文本消息
 
     async def terminate(self):
         """可选择实现异步的插件销毁方法，当插件被卸载/停用时会调用。"""
